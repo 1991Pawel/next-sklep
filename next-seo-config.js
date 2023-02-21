01e57b9bdd0bb1dc8ffa17498d02fab909d827c6
Paweł Leśnik <pawel.lesnik@tdy.pl>
2023-02-21 13:58:02 +0100

    add seo

---
+++ next-seo-config.js
@@ -0,0 +1,22 @@
+const title = "Nasz sklep od zaiste programuj";
+const description = "Nasz sklep od zaiste programuj";
+
+const nextSeoConfig = {
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    images: [
+      {
+        url: "",
+        width: 800,
+        height: 600,
+        alt: "data.thumbnailAlt",
+        type: "image/jpeg",
+      },
+    ],
+    siteName: "Sklep",
+  },
+};
+export default nextSeoConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import SEO from "../next-seo-config";
+import { DefaultSeo } from "next-seo";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
       </QueryClientProvider>
